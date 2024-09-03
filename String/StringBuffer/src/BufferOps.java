e869fcdb3c5ad9ec4f49fcc4d5900f22595f2303
Chavan, Rohan <101328+503301117@users.noreply.github.build.ge.com>
2024-09-03 10:12:33 +0530

    String Update

--- String/StringBuffer/src/BufferOps.java
+++ String/StringBuffer/src/BufferOps.java
@@ -4,25 +4,28 @@ public class BufferOps {
     public static void main(String[] args) {
         String[] subjectArray=new String[3];
         String[] marksArray=new String[3];
+        int sum=0;
         for(int i=0;i<3;i++){
-            System.out.println("Enter the Subject "+(i+1)+"\n");
+            System.out.println("Enter the Subject "+(i+1));
             Scanner sc=new Scanner(System.in);
-            subjectArray[i]=sc.nextLine();
-            System.out.println("Enter Marks of Subject "+subjectArray[i]+"\n");
-            marksArray[i]=sc.nextLine();
+            subjectArray[i]=sc.next();
+            System.out.println("Enter Marks of "+subjectArray[i]+"\n");
+            marksArray[i]=sc.next();
+            sum+=Integer.parseInt(marksArray[i]);
         }
         System.out.println("=======X=X=X=X=X=======");
         StringBuffer finalString=new StringBuffer();
         for(int j=0;j<3;j++){
             int append=marksArray[j].length()+subjectArray[j].length();
-            finalString=finalString.append(subjectArray[j]);
+            finalString.append(subjectArray[j]);
             for(int i=0;i<15-append;i++){
-                finalString=finalString.append(" ");
+                finalString.append(" ");
             }
-            finalString=finalString.append(marksArray[j]);
+            finalString.append(marksArray[j]);
             System.out.println("\t"+finalString);
             finalString.delete(0,finalString.length());
         }
         System.out.println("=======X=X=X=X=X=======");
+        System.out.println("\tTOTAL        "+sum);
     }
 }
